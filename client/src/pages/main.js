f9af4102236ce6fd37196c22a981e68793efb3df
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2021-02-01 16:29:21 +0400

    feat(c/main) added new noice style and loading

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,18 +1,32 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
+import Loading from '../components/loading';
 import routes from '../routes';
 import styles from './style.module.scss';
 
 export default function Main() {
+  const [isLoad, setIsLoad] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoad(false), 3000);
+  }, []);
+
   return (
     <div className={styles.root}>
-      <Header />
-      <div className={styles.body}>
-        <Sidebar />
-        <main className={styles.main}>
-          {routes()}
-        </main>
-      </div>
+      {isLoad
+        ? <Loading />
+        : (
+          <>
+            <Header />
+            <div className={styles.body}>
+              <Sidebar />
+              <main className={styles.main}>
+                {routes()}
+              </main>
+            </div>
+          </>
+        )}
     </div>
   );
 }
