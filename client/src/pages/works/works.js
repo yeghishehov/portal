4f99bd04b3299098c3e88c51d74eb79fab45087e
Yeghishe Hovhannisyan <yeghishehov.1@gmail.com>
2021-02-02 18:27:41 +0400

    feat(c/works) added tools

--- client/src/pages/works/works.js
+++ client/src/pages/works/works.js
@@ -1,7 +1,22 @@
+import styles from './style.module.scss';
+
 export default function Works() {
   return (
-    <div style={{ color: 'white' }}>
-      Works
+    <div className={styles.root}>
+      <div className={styles.tools}>
+        <label htmlFor="search" className={styles.label}>
+          <div className={styles.labelText}>search</div>
+          <input type="text" id="search" className={styles.input} />
+        </label>
+        <label htmlFor="categories" className={styles.label}>
+          <div className={styles.labelText}>categories</div>
+          <input type="text" id="categories" className={styles.input} />
+        </label>
+        <label htmlFor="filters" className={styles.label}>
+          <div className={styles.labelText}>filters</div>
+          <input type="text" id="filters" className={styles.input} />
+        </label>
+      </div>
     </div>
   );
 }
